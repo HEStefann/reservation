3ceb526295ac46fb1782d246c7771542517fee04
HEStefann <stefanhadzieftimov@gmail.com>
2023-08-09 09:15:08 +0200

    updating routes

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/restaurants', [UserController::class, 'index'])->name('user.restaurants');
-
-Route::get('/history', [ReservationController::class, 'history'])->name('history');
-
-
 Route::get('/dashboard', [RestaurantController::class, 'dashboard'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -44,10 +39,9 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-
 Route::middleware('auth')->group(function () {
     // Restaurant registration routes
-    Route::get('/restaurant/register', [RestaurantController::class, 'create'])->name('restaurant.register'); // routes/web.php
+    Route::get('/restaurant/register', [RestaurantController::class, 'create'])->name('restaurant.register');
     Route::post('/restaurant/register', [RestaurantController::class, 'store']);
 
     // Restaurant settings routes
@@ -58,43 +52,29 @@ Route::middleware('auth')->group(function () {
         Route::post('/update-operating-hours', [RestaurantSettingsController::class, 'updateOperatingHours'])->name('update_operating_hours');
         Route::post('/update-operating-status', [RestaurantSettingsController::class, 'updateOperatingStatus'])->name('update_operating_status');
         Route::post('/update-content', [RestaurantSettingsController::class, 'updateContent'])->name('update_content');
+
+        // Working hours routes
+        Route::prefix('/working-hours')->name('working-hours.')->group(function () {
+            Route::get('/{date}', [RestaurantSettingsCalendarController::class, 'getWorkingHoursForDate'])->name('get');
+            Route::put('/', [RestaurantSettingsCalendarController::class, 'updateWorkingHours'])->name('update');
+        });
+
+        // Restaurant images routes
+        Route::get('/images', [RestaurantImageController::class, 'index']);
+        Route::post('/images', [RestaurantImageController::class, 'upload'])->name('restaurant.image.upload');
+
+        // Restaurant tags route
+        Route::post('/tags', [RestaurantTagsController::class, 'update'])->name('restaurant.tags.update');
     });
 
-    Route::get('/restaurant/{restaurant}/images', [RestaurantImageController::class, 'index']);
-    Route::post('/restaurant/{restaurant}/images', [RestaurantImageController::class, 'upload'])->name('restaurant.image.upload');
-
     Route::resource('tags', TagsController::class);
-    Route::post('/restaurant/{restaurant}/tags', [RestaurantTagsController::class, 'update'])->name('restaurant.tags.update');
 
     // Calendar routes
     Route::get('/calendar/{date?}', [RestaurantSettingsCalendarController::class, 'calendar']);
 
-    // Working hours routes
-    Route::prefix('/restaurant/{restaurant}/working-hours')->name('restaurant.working-hours.')->group(function () {
-        Route::get('/{date}', [RestaurantSettingsCalendarController::class, 'getWorkingHoursForDate'])->name('get');
-        Route::put('/', [RestaurantSettingsCalendarController::class, 'updateWorkingHours'])->name('update');
-    });
+    Route::get('/dashboard', [ReservationController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
+    Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
+    Route::get('/user/restaurants', [UserController::class, 'index'])->name('restaurants.index');
+    Route::get('/history', [ReservationController::class, 'history'])->name('history');
 });
-
-Route::get('/dashboard', [ReservationController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
-Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
-Route::get('/user/restaurants', [UserController::class, 'index'])->name('restaurants.index');
-
-
-
-    // Route::get('/restaurant/{restaurant}/settings', [RestaurantSettingsController::class, 'index'])->name('restaurant.settings.index');
-
-    // Route::post('/restaurant/{restaurant}/working-hours/update', [RestaurantController::class, 'updateWorkingHours'])
-    // ->name('restaurant.working-hours.update'); 
-
-    // // Restaurant image route
-    // Route::post('/restaurant/{restaurant}/upload-image', [RestaurantImageController::class, 'upload'])->name('restaurant.image.upload');
-
-    // // Restaurant tag route
-    // Route::post('/restaurant/{restaurant}/update-tags', [RestaurantSettingsController::class, 'updateTags'])->name('restaurant.tags.update');
-
-    // // Calendar routes
-    // Route::get('/calendar/{date?}', [RestaurantSettingsCalendarController::class, 'calendar']);
-    // Route::get('/calendar', [CalendarController::class, 'index']);
-    // Route::post('/calendar/update-day', [CalendarController::class, 'updateDay']);

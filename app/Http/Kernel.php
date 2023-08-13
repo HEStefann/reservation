3025a7ec0338eb70de4db1f2907c95531bb65e19
HEStefann <stefanhadzieftimov@gmail.com>
2023-08-13 19:13:16 +0200

    middleware fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'GuestRole' => \App\Http\Middleware\GuestRole::class,
+        'ModeratorRole' => \App\Http\Middleware\moderatorRole::class,
+        'OwnerRole' => \App\Http\Middleware\ownerRole::class
     ];
 }
